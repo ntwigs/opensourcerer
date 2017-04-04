19c66e5c0f34cfba0374c6e12efc580b4c5a0f5c
Oscar Nordquist <on222bx@student.lnu.se>
2017-04-04 23:20:21 +0200

    added success roues

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ dotenv.config()
 import express from 'express'
 import mongoDB from './utils/mongoDB'
 import mainRoute from './routes/mainRoute'
+import authRoute from './routes/authRoute'
 
 const app = express()
 const PORT = 3000
 
 app.use(mainRoute)
+app.use(authRoute)
 
 app.listen(PORT, () => {
     console.log(`Up and running on port : ${ PORT }`)
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,9 +1,13 @@
 import express from 'express'
+import passport from 'passport'
 const router = express.Router()
 
 router
-    .get('/login', (req, res) => {
-        res.send('login')
-    })
+    .get('/login', passport.authenticate('github'))
+    .get('/login/callback',
+      passport.authenticate('github'),
+      (req, res) => {
+        res.redirect('/')
+      })
 
 export default router
