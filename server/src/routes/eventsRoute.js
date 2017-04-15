53619161432d47a27fa9f6de6546bf6f5ca55ee9
Oscar Nordquist <on222bx@student.lnu.se>
2017-04-15 19:45:24 +0200

    server: displays events for unregistered users

--- server/src/routes/eventsRoute.js
+++ server/src/routes/eventsRoute.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import passport from 'passport'
+import rp from 'request-promise'
 import UserSchema from '../schemas/UserSchema'
 const router = express.Router()
 
@@ -10,6 +11,33 @@ router
       const user = await UserSchema.findOne(
         { username: { $regex: username, $options: 'i' } }
       )
+
+      if (!user) {
+        const events = await rp(`https://api.github.com/users/${ username }/events`, {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${ process.env.GITHUB_ACCESS_TOKEN }`,
+            'User-Agent': 'NorthernTwig'
+          },
+          json: true
+        })
+
+        const organizedEvents = events.map(event => {
+          return  {
+            id: event.id,
+            type: event.type,
+            repo: event.repo.name
+          }
+        })
+
+        console.log(organizedEvents)
+
+        return res.json({
+          organizedEvents
+        })
+
+      }
+
       const organizedEvents = user.events.map(event => {
         return  {
           id: event.id,
