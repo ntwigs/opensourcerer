544178d7a70310315bdc804ae0adc213a02a5346
Oscar Nordquist <on222bx@student.lnu.se>
2017-04-16 18:01:26 +0200

    client: styled-component cleanup

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -7,12 +7,12 @@ export default class extends Component {
 
     return (
       <Item>
-        <EventContainer className='item content large'>
+        <EventContainer className='large'>
           <RepoImage src='https://github.com/identicons/osc.png'></RepoImage>
           <TextContainer className='text-container'>
             <h3>{ type }</h3>
             <h2>MERGED</h2>
-            <h1>Exp: +{ Math.ceil(id/100000000) }</h1>
+            <h1>+{ Math.ceil(id/100000000) }</h1>
           </TextContainer>
         </EventContainer>
       </Item>
@@ -20,29 +20,13 @@ export default class extends Component {
   }
 }
 
+
+
 const Item = styled.div`
   box-sizing: border-box;
   break-inside: avoid;
   counter-increment: item-counter;
   padding: 10px;
-
-  .content {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background: #282840;
-    border-radius: 2px;
-    box-sizing: border-box;
-    padding: 10px;
-    box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 1px 5px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0.2);
-  }
-  
-  .text-container {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-    }
   
   img {
     width: 75px;
@@ -65,19 +49,11 @@ const Item = styled.div`
   }
   
   .large {
-    position: relative;
-    display: flex;
     flex-direction: column;
     align-items: center;
-    background: #282840;
-    border-radius: 2px;
-    box-sizing: border-box;
-    padding: 10px;
     
     .text-container {
       margin-top: 20px;
-      display: flex;
-      flex-direction: column;
       align-items: center;
     }
     
@@ -85,25 +61,24 @@ const Item = styled.div`
   
   .small {
     height: 100px;
-    position: relative;
-    display: flex;
     flex-direction: row;
-    background: #282840;
-    border-radius: 2px;
-    box-sizing: border-box;
     justify-content: space-between;
-    padding: 10px;
     
     .text-container {
-      display: flex;
-      flex-direction: column;
       align-items: flex-end;
     }
   }
 `
 
 const EventContainer = styled.div`
-
+    position: relative;
+    display: flex;
+    align-items: center;
+    background: #282840;
+    border-radius: 2px;
+    box-sizing: border-box;
+    padding: 10px;
+    box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 1px 5px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0.2);
 `
 
 const RepoImage = styled.img`
@@ -113,27 +88,8 @@ const RepoImage = styled.img`
 
 const TextContainer = styled.div`
   display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  width: 100%;
-  height: 75px;
-`
-
-const RightTextField = styled.div`
-  display: flex;
-  width: 60%;
-  height: 100%;
-  align-items: flex-end;
   flex-direction: column;
-  justify-content: space-between;
-`
-
-const LeftTextField = styled.div`
-  display: flex;
-  width: 100%;
-  height: 100%;
-  align-items: flex-start;
-  flex-direction: column;
+  align-items: center;
 `
 
 const Time = styled.h6`
