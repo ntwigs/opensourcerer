7eee6b78bb5cf4c0e2374927ed9fc68bc166e30d
Oscar Nordquist <on222bx@student.lnu.se>
2017-04-16 18:38:35 +0200

    server: added a event cleaner

---
+++ server/src/utils/eventCleaner.js
@@ -0,0 +1,46 @@
+export default event => {
+  switch(event.type) {
+    case 'IssueEvent':
+      return {
+        name: 'Issue',
+        action: event.payload.action,
+        experience: 175 
+      }
+    case 'IssueCommentEvent':
+      return {
+        name: 'Issue Comment',
+        action: event.payload.action,
+        experience: 175 
+      }
+    case 'PushEvent':
+      return {
+        name: 'Push',
+        action: 'To Repo',
+        experience: 25
+      }
+    case 'PublicEvent':
+      return {
+        name: 'Repo',
+        action: 'Public',
+        experience: 500
+      }
+    case 'PullRequestEvent':
+      return {
+        name: 'Pull Request',
+        action: event.payload.action,
+        experience: 200
+      }
+    case 'ForkEvent': 
+      return {
+        name: 'Forked',
+        action: event.payload.forkee.name,
+        experience: 50
+      }
+    case 'PullRequestReviewCommentEvent':
+      return {
+        name: 'Review Comment',
+        action: event.payload.action,
+        experience: 125
+      }
+  }
+}
