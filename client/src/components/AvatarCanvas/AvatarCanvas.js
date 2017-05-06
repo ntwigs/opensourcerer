fd4c34f9e908568607d4b3eec834b4ceedd0606c
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-05-06 10:15:44 +0200

    client: removed canvas testing code

--- client/src/components/AvatarCanvas/AvatarCanvas.js
+++ client/src/components/AvatarCanvas/AvatarCanvas.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react'
 
 export default class extends Component {
-  state = {
-    x: 0,
-    y: 100
-  }
-
   componentDidMount = () => {
     this.canvas = this.canvasReference
     this.ctx = this.canvas.getContext('2d')
@@ -15,18 +10,12 @@ export default class extends Component {
   clear = () => {
     this.ctx.clearRect(0, 0, 500, 500)
   }
-
-  drawSquare = () => {
-    this.ctx.fillStyle = 'rgba(0, 0, 0, 0.95)'
-    this.ctx.fillRect(this.state.x, this.state.y, 20, 20)
-    this.setState({
-      x: this.state.x + 0.5
-    })
-  }
-
+  
   update = () => {
     this.clear()
-    this.drawSquare()
+
+    // Animation goes here
+
     requestAnimationFrame(() => this.update())
   }
 
