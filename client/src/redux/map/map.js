651942a880ae1eab0074d579339f2fbc0d191e31
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-05-06 11:09:10 +0200

    client: added hat actions to map

--- client/src/redux/map/map.js
+++ client/src/redux/map/map.js
@@ -1,9 +1,11 @@
 import { bindActionCreators } from 'redux'
 import * as userActions from '../actions/user'
 import * as inventoryActions from '../actions/inventory'
+import * as avatarCanvasActions from '../actions/avatarCanvas'
 
 export const mapDispatchToProps = dispatch => bindActionCreators({
   ...userActions,
-  ...inventoryActions
+  ...inventoryActions,
+  ...avatarCanvasActions
 }, dispatch)
 export const mapStateToProps = state => ({ state })
