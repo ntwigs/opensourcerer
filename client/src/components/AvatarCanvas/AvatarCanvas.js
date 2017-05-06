1d992b7ab55fc98b1eeb9f81afced841930ead02
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-05-06 10:49:18 +0200

    client: connected and rendered AvatarCanvas

--- client/src/components/AvatarCanvas/AvatarCanvas.js
+++ client/src/components/AvatarCanvas/AvatarCanvas.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { mapDispatchToProps, mapStateToProps } from '../../redux/map/map'
 import Hat from './Hat'
+import Avatar from './Avatar'
 
-export default class extends Component {
+class AvatarCanvas extends Component {
   componentDidMount = () => {
     this.canvas = this.canvasReference
     this.ctx = this.canvas.getContext('2d')
     this.hat = new Hat(0, 0, this.ctx)
+    this.avatar = new Avatar(0, 0, this.ctx, this.props.state.user.avatarUrl)
+    this.width = 500
+    this.height = 500
     this.update()
   }
 
-  clear = () => {
-    this.ctx.clearRect(0, 0, 500, 500)
-  }
-  
   update = () => {
     this.clear()
-
+    this.avatar.render()
+    
     if (this.hat) {
       this.renderHat()
     }
 
     requestAnimationFrame(() => this.update())
+  }
+
+  clear = () => {
+    this.ctx.clearRect(0, 0, this.width, this.height)
   }
 
   renderHat = () => {
@@ -37,3 +44,5 @@ export default class extends Component {
     )
   }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(AvatarCanvas)
