299aae2fd84adc5a80acb3edf105a7e87219276e
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-05-06 11:09:53 +0200

    client: added new reducer to other reducers

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import user from './user'
 import inventory from './inventory'
+import avatarCanvas from './inventory'
 
 export default combineReducers({
   user,
-  inventory
+  inventory,
+  avatarCanvas
 })
