71bf168748db7b608a77b0ff7a8c0de8839ceada
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-05-08 10:01:50 +0200

    client: settings x-orgin to hats in canvas

--- client/src/components/AvatarCanvas/Hat.js
+++ client/src/components/AvatarCanvas/Hat.js
@@ -2,11 +2,15 @@ export default class {
   constructor(x, y, ctx, src) {
     this.x = x
     this.y = y
+    this.displayTools = false
     this.ctx = ctx
     this.hat = new Image()
+    this.hat.crossOrigin = 'anonymous'
     this.hat.src = src
     this.selected = false
   }
+
+  
 
   render() {
     this.ctx.drawImage(this.hat, this.x, this.y)
