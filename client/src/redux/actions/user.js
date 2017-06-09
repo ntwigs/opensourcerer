b104b86c7e046b6aa1f2c261534f97b649d25873
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-09 22:17:17 +0200

    Added userupdate action

--- client/src/redux/actions/user.js
+++ client/src/redux/actions/user.js
@@ -1,4 +1,11 @@
+import { getInitialEvents } from '../../lib/http'
+
 export const experienceUpdate = experience => ({ type: 'EXPERIENCE_UPDATE', experience })
 export const levelUpdate = level => ({ type: 'LEVEL_UPDATE', level })
 export const avatarUpdate = avatarUrl => ({ type: 'AVATAR_UPDATE', avatarUrl })
 export const titleUpdate = title => ({ type: 'TITLE_UPDATE', title })
+
+export const updateUserData = async (username, etag) => {
+  const result = await getInitialEvents(username)
+  return dispatch => dispatch({ type: 'UPDATE_USER_DATA', payload: { result, etag } })
+}
