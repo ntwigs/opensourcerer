2e2f3b22535038a18d1d2ebd483af5d854556033
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-10 16:06:55 +0200

    Client: added action for fetching new events

--- client/src/redux/actions/user.js
+++ client/src/redux/actions/user.js
@@ -1,4 +1,4 @@
-import { getInitialEvents } from '../../lib/http'
+import { getInitialEvents, getNewEvents } from '../../lib/http'
 
 export const experienceUpdate = experience => ({ type: 'EXPERIENCE_UPDATE', experience })
 export const levelUpdate = level => ({ type: 'LEVEL_UPDATE', level })
@@ -8,4 +8,9 @@ export const titleUpdate = title => ({ type: 'TITLE_UPDATE', title })
 export const updateUserData = async (username, etag) => {
   const result = await getInitialEvents(username)
   return dispatch => dispatch({ type: 'UPDATE_USER_DATA', payload: { result, etag } })
+}
+
+export const userLevelup = async (username, etag) => {
+  const result = await getNewEvents(username)
+  return dispatch => dispatch({ type: 'USER_LEVELUP', payload: { result, etag } })
 }
