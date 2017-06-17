8be67027037f479834dd42d31b7f4bd49ba4e398
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-17 11:38:28 +0200

    Client: added colors file

---
+++ client/src/lib/colors.js
@@ -0,0 +1,9 @@
+// BACKGROUND
+export const BACKGROUND_BACKDROP = '#1E202F'
+export const BACKGROUND = '#282840'
+
+// TEXT
+export const TEXT_LIGHT = '#DEDDED'
+export const TEXT_DARK = '#1E202F'
+export const TEXT_HIGHLIGHT = '#6A93FF'
+
